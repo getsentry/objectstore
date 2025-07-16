6fca8d0043e6bb316e490cf3545e3405a029a5c3
Arpad Borsos <arpad.borsos@sentry.io>
2025-07-16 11:40:23 +0200

    Replace tonic with prost for building protobuf types

--- objectstore-api/build.rs
+++ objectstore-api/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/storage.proto")?;
+    prost_build::compile_protos(&["proto/storage.proto"], &["proto"])?;
     Ok(())
 }
