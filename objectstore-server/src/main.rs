193d0dd1450690a3b9e9573d0163c18cd0d3d69b
Jan Michael Auer <mail@jauer.org>
2025-07-16 09:53:14 +0200

    fix: Outdated reference to service crate

--- objectstore-server/src/main.rs
+++ objectstore-server/src/main.rs
@@ -1,8 +1,8 @@
 //! The storage server component.
 //!
-//! This builds on top of the [`service`], and exposes the underlying
-//! storage layer as both a `gRPC` service for use by the `client`, as well as
-//! an `HTTP` layer which can serve files directly to *external clients*.
+//! This builds on top of the [`objectstore-service`], and exposes the underlying storage layer as
+//! both a `gRPC` service for use by the `client`, as well as an `HTTP` layer which can serve files
+//! directly to *external clients*.
 
 use std::sync::Arc;
 
