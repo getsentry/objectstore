1a77d7b8d5eef12c0433ad79e7016287da61447d
Arpad Borsos <arpad.borsos@sentry.io>
2025-08-06 15:04:35 +0200

    Implement metadata for the FS backend
    This is just serializing and putting metadata in front of the actual payload.
    
    It also removes all the other code that was still in there dealing with a similar idea, which was also more about splitting objects into parts, etc.

--- objectstore-service/src/backend/local_fs.rs
+++ objectstore-service/src/backend/local_fs.rs
@@ -2,13 +2,16 @@ use std::io::ErrorKind;
 use std::path::{Path, PathBuf};
 use std::pin::pin;
 
+use bincode::error::DecodeError;
 use futures_util::StreamExt;
 use objectstore_types::Metadata;
 use tokio::fs::OpenOptions;
-use tokio::io::{AsyncWriteExt, BufWriter};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio_util::io::{ReaderStream, StreamReader};
 
 use super::{Backend, BackendStream};
+
+const BC_CONFIG: bincode::config::Configuration = bincode::config::standard();
 
 #[derive(Debug)]
 pub struct LocalFs {
@@ -26,7 +29,7 @@ impl Backend for LocalFs {
     async fn put_object(
         &self,
         path: &str,
-        _metadata: &Metadata,
+        metadata: &Metadata,
         stream: BackendStream,
     ) -> anyhow::Result<()> {
         let path = self.path.join(path);
@@ -39,6 +42,9 @@ impl Backend for LocalFs {
 
         let mut reader = pin!(StreamReader::new(stream));
         let mut writer = BufWriter::new(file);
+
+        let metadata = bincode::serde::encode_to_vec(metadata, BC_CONFIG)?;
+        writer.write_all(&metadata).await?;
 
         tokio::io::copy(&mut reader, &mut writer).await?;
         writer.flush().await?;
@@ -59,12 +65,80 @@ impl Backend for LocalFs {
             err => err?,
         };
 
-        let stream = ReaderStream::new(file);
-        Ok(Some((Default::default(), stream.boxed())))
+        let mut reader = BufReader::new(file);
+        let mut metadata_buf = vec![];
+        let metadata = loop {
+            let reader_buf = reader.fill_buf().await?;
+            let buf = if metadata_buf.is_empty() {
+                reader_buf
+            } else {
+                metadata_buf.extend_from_slice(reader_buf);
+                &metadata_buf
+            };
+
+            match bincode::serde::decode_from_slice(buf, BC_CONFIG) {
+                Ok((metadata, read)) => {
+                    let read = if metadata_buf.is_empty() {
+                        read
+                    } else {
+                        let prev_consumed = metadata_buf.len() - reader_buf.len();
+                        read - prev_consumed
+                    };
+                    reader.consume(read);
+                    break metadata;
+                }
+                Err(DecodeError::UnexpectedEnd { .. }) => {
+                    metadata_buf.extend_from_slice(reader_buf);
+                    let len = reader_buf.len();
+                    reader.consume(len);
+                }
+                Err(err) => Err(err)?,
+            }
+        };
+
+        let stream = ReaderStream::new(reader);
+        Ok(Some((metadata, stream.boxed())))
     }
 
     async fn delete_object(&self, path: &str) -> anyhow::Result<()> {
         let path = self.path.join(path);
         Ok(tokio::fs::remove_file(path).await?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use bytes::BytesMut;
+    use futures_util::TryStreamExt;
+    use objectstore_types::{Compression, ExpirationPolicy};
+
+    use super::*;
+
+    fn make_stream(contents: &[u8]) -> BackendStream {
+        tokio_stream::once(Ok(contents.to_vec().into())).boxed()
+    }
+
+    #[tokio::test]
+    async fn stores_metadata() {
+        let tempdir = tempfile::tempdir().unwrap();
+        let backend = LocalFs::new(tempdir.path());
+
+        let metadata = Metadata {
+            expiration_policy: ExpirationPolicy::TimeToIdle(Duration::from_secs(3600)),
+            compression: Some(Compression::Zstd),
+            custom: [("foo".into(), "bar".into())].into(),
+        };
+        backend
+            .put_object("foo/bar", &metadata, make_stream(b"oh hai!"))
+            .await
+            .unwrap();
+
+        let (read_metadata, stream) = backend.get_object("foo/bar").await.unwrap().unwrap();
+        let file_contents: BytesMut = stream.try_collect().await.unwrap();
+
+        assert_eq!(read_metadata, metadata);
+        assert_eq!(file_contents.as_ref(), b"oh hai!");
     }
 }
