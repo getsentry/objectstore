getsentry/objectstore

commit f1ab72713988032b0bd2a402b68705194f5e9465

fix: Initialize Sentry before tokio (#97)
The way Sentry does its Hub/Client-propagation across threads means that
it has to be initialized on the main thread for this to work, before any
other threads are being spawned.

In other words, Sentry is notorious for being incompatible with
`#[tokio::main]`. So lets split the main function into a sync portion
initializing all the observability crates, before spawning the tokio
runtime and the main server.

objectstore-server/src/main.rs
@@ -17,35 +17,37 @@ mod http;
 mod observability;
 mod state;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let config = Config::from_env()?;
+    tracing::debug!(?config, "Starting service");
 
     // Ensure a rustls crypto provider is installed, required on distroless.
     rustls::crypto::ring::default_provider()
         .install_default()
         .expect("Failed to install rustls crypto provider");
 
-    let metrics_guard = maybe_initialize_metrics(&config)?;
     let _sentry_guard = maybe_initialize_sentry(&config);
+    let metrics_guard = maybe_initialize_metrics(&config)?;
     initialize_tracing(&config);
 
-    tracing::debug!(?config, "Starting service");
-    let state = State::new(config).await?;
-    tokio::spawn(http::server(state));
+    let runtime = tokio::runtime::Runtime::new()?;
+    runtime.block_on(async move {
+        let state = State::new(config).await?;
+        tokio::spawn(http::server(state));
 
-    elegant_departure::tokio::depart()
-        .on_termination()
-        .on_sigint()
-        .on_signal(SignalKind::hangup())
-        .on_signal(SignalKind::quit())
-        .await;
+        elegant_departure::tokio::depart()
+            .on_termination()
+            .on_sigint()
+            .on_signal(SignalKind::hangup())
+            .on_signal(SignalKind::quit())
+            .await;
 
-    if let Some(metrics_guard) = metrics_guard {
-        metrics_guard.flush(None).await?;
-    }
+        if let Some(metrics_guard) = metrics_guard {
+            metrics_guard.flush(None).await?;
+        }
 
-    tracing::info!("shutting down");
+        tracing::info!("shutting down");
 
-    Ok(())
+        Ok(())
+    })
 }

commit 471680ddb4f8a74e7ae5266f79835311d8b38aa2

fix(server): Enter runtime to allow for datadog initialization (#100)
Fixes a regression in #97 that causes a panic during startup when
Datadog is configured.

objectstore-server/src/main.rs
@@ -18,6 +18,9 @@ mod observability;
 mod state;
 
 fn main() -> Result<()> {
+    let runtime = tokio::runtime::Runtime::new()?;
+    let _runtime_guard = runtime.enter();
+
     let config = Config::from_env()?;
     tracing::debug!(?config, "Starting service");
 
@@ -30,7 +33,6 @@ fn main() -> Result<()> {
     let metrics_guard = maybe_initialize_metrics(&config)?;
     initialize_tracing(&config);
 
-    let runtime = tokio::runtime::Runtime::new()?;
     runtime.block_on(async move {
         let state = State::new(config).await?;
         tokio::spawn(http::server(state));