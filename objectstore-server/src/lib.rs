a00f8954be2e0b26d9c79596dc2524b826134412
Arpad Borsos <arpad.borsos@sentry.io>
2025-09-29 09:24:12 +0200

    chore: Actually test against the real server (#114)
    We keep breaking the server functionality and the stresstests. Even
    though we run the stresstest in a test, it does not catch errors, as it
    will just report failed requests, which are swallowed in its output.
    
    Instead, we can reuse the client tests to actually run against the
    *real* server, instead of a manually written mock server.

---
+++ objectstore-server/src/lib.rs
@@ -0,0 +1,9 @@
+//! The storage server component.
+//!
+//! This builds on top of the [`objectstore-service`], and exposes the underlying storage layer as
+//! an `HTTP` layer which can serve files directly to *external clients* and our SDK.
+
+pub mod config;
+pub mod http;
+pub mod observability;
+pub mod state;
