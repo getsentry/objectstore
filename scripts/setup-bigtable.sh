7ef142f440db096242117d56f3caf139a1e756a9
Jan Michael Auer <mail@jauer.org>
2025-10-14 13:53:18 +0200

    fix: Container name for setup-bigtable script

--- scripts/setup-bigtable.sh
+++ scripts/setup-bigtable.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker exec objectstore-bigtable-1 bash -c "
+CONTAINER_NAME=${1:-objectstore-bigtable-1}
+
+docker exec "$CONTAINER_NAME" bash -c "
 export BIGTABLE_EMULATOR_HOST=localhost:8086
 cbt -project testing -instance objectstore deletetable objectstore || true
 cbt -project testing -instance objectstore createtable objectstore families=fg,fm
