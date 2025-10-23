getsentry/objectstore

commit 6696e9b39f6dad30830c4d45e43b28ac885dbefc

ref(server): Create an app struct and improve inflight requests (#140)
Moves the router into an `App` struct that has a constructor and a
`serve` method that starts the `axum` server future. This also
simplifies usage in tests, as it is no longer required to import axum
directly.

Internally, `App::serve` will start all concurrent processes that the
app needs. Initially, this is just the web server and the emitter from
the in-flight requests middleware. Potentially, it is cleaner to spawn
them into separate tasks, but since we spawn externally right now I
thought it's better that this future has no side effects and can be
canceled at once.

Additionally, this improves the handling of in-flight requests. We now
use the utility from `tower-http` with a built-in drop guard for more
accurate counting. Since this requires an emitter task to run, we start
it simultaneously with the axum server and await both before finishing
the serve future.

objectstore-server/src/http.rs
@@ -1,7 +1,7 @@
 use std::any::Any;
 use std::io;
 use std::net::SocketAddr;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
 
 use anyhow::Result;
 use axum::body::Body;
@@ -20,6 +20,8 @@ use tokio::net::{TcpListener, TcpSocket};
 use tokio::time::Instant;
 use tower::ServiceBuilder;
 use tower_http::catch_panic::CatchPanicLayer;
+use tower_http::metrics::InFlightRequestsLayer;
+use tower_http::metrics::in_flight_requests::InFlightRequestsCounter;
 use tower_http::set_header::SetResponseHeaderLayer;
 use tower_http::trace::{DefaultOnFailure, TraceLayer};
 use tracing::Level;
@@ -27,49 +29,111 @@ use tracing::Level;
 use crate::config::Config;
 use crate::state::ServiceState;
 
+/// The maximum backlog for TCP listen sockets before refusing connections.
 const TCP_LISTEN_BACKLOG: u32 = 1024;
+
+/// Interval for emitting the in-flight requests gauge metric.
+const IN_FLIGHT_INTERVAL: Duration = Duration::from_secs(1);
+
+/// The value for the `Server` HTTP header.
 const SERVER: &str = concat!("objectstore/", env!("CARGO_PKG_VERSION"));
 
-static IN_FLIGHT_REQUESTS: AtomicUsize = AtomicUsize::new(0);
-
-pub type App = axum::Router;
-
-#[derive(Deserialize, Debug)]
-struct ContextParams {
-    pub scope: String,
-    pub usecase: String,
+/// The objectstore web server application.
+#[derive(Debug)]
+pub struct App {
+    router: axum::Router,
+    in_flight_requests: InFlightRequestsCounter,
+    graceful_shutdown: bool,
 }
 
-pub fn make_app(state: ServiceState) -> App {
-    // Build the router middleware into a single service which runs _after_ routing. Service
-    // builder order defines layers added first will be called first. This means:
-    //  - Requests go from top to bottom
-    //  - Responses go from bottom to top
-    let middleware = ServiceBuilder::new()
-        .layer(axum::middleware::from_fn(emit_request_metrics))
-        .layer(CatchPanicLayer::custom(handle_panic))
-        .layer(SetResponseHeaderLayer::overriding(
-            header::SERVER,
-            HeaderValue::from_static(SERVER),
-        ))
-        .layer(NewSentryLayer::new_from_top())
-        .layer(SentryHttpLayer::new().enable_transaction())
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(make_http_span)
-                .on_failure(DefaultOnFailure::new().level(Level::DEBUG)),
+impl App {
+    /// Creates a new application router for the given service state.
+    ///
+    /// The applications sets up middlewares and routes for the objectstore web API. Use
+    /// [`serve`](Self::serve) to run the server future.
+    pub fn new(state: ServiceState) -> Self {
+        let (in_flight_layer, in_flight_requests) = InFlightRequestsLayer::pair();
+
+        // Build the router middleware into a single service which runs _after_ routing. Service
+        // builder order defines layers added first will be called first. This means:
+        //  - Requests go from top to bottom
+        //  - Responses go from bottom to top
+        let middleware = ServiceBuilder::new()
+            .layer(axum::middleware::from_fn(emit_request_metrics))
+            .layer(in_flight_layer)
+            .layer(CatchPanicLayer::custom(handle_panic))
+            .layer(SetResponseHeaderLayer::overriding(
+                header::SERVER,
+                HeaderValue::from_static(SERVER),
+            ))
+            .layer(NewSentryLayer::new_from_top())
+            .layer(SentryHttpLayer::new().enable_transaction())
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(make_http_span)
+                    .on_failure(DefaultOnFailure::new().level(Level::DEBUG)),
+            );
+
+        let service_routes = Router::new().route("/", put(put_object_nokey)).route(
+            "/{*key}",
+            put(put_object).get(get_object).delete(delete_object),
         );
 
-    let service_routes = Router::new().route("/", put(put_object_nokey)).route(
-        "/{*key}",
-        put(put_object).get(get_object).delete(delete_object),
-    );
+        let router = Router::new()
+            .nest("/v1/", service_routes)
+            .route("/health", get(health))
+            .layer(middleware)
+            .with_state(state);
 
-    Router::new()
-        .nest("/v1/", service_routes)
-        .route("/health", get(health))
-        .layer(middleware)
-        .with_state(state)
+        App {
+            router,
+            in_flight_requests,
+            graceful_shutdown: false,
+        }
+    }
+
+    /// Enables or disables graceful shutdown for the server.
+    ///
+    /// By default, graceful shutdown is disabled.
+    pub fn graceful_shutdown(mut self, enable: bool) -> Self {
+        self.graceful_shutdown = enable;
+        self
+    }
+
+    /// Runs the web server until graceful shutdown is triggered.
+    ///
+    /// This function creates a future that runs the server. The future must be spawned or awaited for
+    /// the server to continue running.
+    pub async fn serve(self, listener: TcpListener) -> Result<()> {
+        let Self {
+            router,
+            in_flight_requests,
+            graceful_shutdown,
+        } = self;
+
+        let service =
+            ServiceExt::<Request>::into_make_service_with_connect_info::<SocketAddr>(router);
+
+        let server = async move {
+            if graceful_shutdown {
+                let guard = elegant_departure::get_shutdown_guard().shutdown_on_drop();
+                axum::serve(listener, service)
+                    .with_graceful_shutdown(guard.wait_owned())
+                    .await
+            } else {
+                axum::serve(listener, service).await
+            }
+        };
+
+        let emitter = in_flight_requests.run_emitter(IN_FLIGHT_INTERVAL, |count| async move {
+            merni::gauge!("server.in_flight_requests": count);
+        });
+
+        let (serve_result, _) = tokio::join!(server, emitter);
+        serve_result?;
+
+        Ok(())
+    }
 }
 
 /// Create a tracing span for an HTTP request.
@@ -121,16 +185,9 @@ async fn emit_request_metrics(mut request: Request, next: Next) -> Response {
     let matched_path = request.extract_parts::<MatchedPath>().await;
     let route = matched_path.as_ref().map_or("unknown", |m| m.as_str());
     let method = request.method().clone();
-
-    // `fetch_add` returns the previous value, so we add 1. We consider this sufficiently accurate
-    // since we only need to get a rough order of magnitude.
-    let in_flight = IN_FLIGHT_REQUESTS.fetch_add(1, Ordering::Relaxed) + 1;
-
     merni::counter!("server.requests": 1, "route" => route, "method" => method.as_str());
-    merni::gauge!("server.requests.in_flight": in_flight);
 
     let response = next.run(request).await;
-    IN_FLIGHT_REQUESTS.fetch_sub(1, Ordering::Relaxed);
 
     merni::distribution!(
         "server.requests.duration"@s: request_start.elapsed(),
@@ -159,23 +216,6 @@ fn listen(config: &Config) -> Result<TcpListener> {
     Ok(listener)
 }
 
-/// Runs a web server until graceful shutdown is triggered.
-///
-///
-/// This function creates a future that runs the server. The future must be spawned or awaited for
-/// the server to continue running.
-///
-/// Use [`make_app`] to create the application router.
-async fn serve(listener: TcpListener, app: App) -> Result<()> {
-    let guard = elegant_departure::get_shutdown_guard().shutdown_on_drop();
-    let service = ServiceExt::<Request>::into_make_service_with_connect_info::<SocketAddr>(app);
-    axum::serve(listener, service)
-        .with_graceful_shutdown(guard.wait_owned())
-        .await?;
-
-    Ok(())
-}
-
 /// Runs the objectstore HTTP server.
 ///
 /// This function creates a future that runs the server. The future must be spawned or awaited for
@@ -184,12 +224,20 @@ pub async fn server(state: ServiceState) -> Result<()> {
     merni::counter!("server.start": 1);
     let listener = listen(&state.config)?;
 
-    let app = make_app(state);
-    serve(listener, app).await
+    App::new(state)
+        .graceful_shutdown(true)
+        .serve(listener)
+        .await
 }
 
 async fn health() -> impl IntoResponse {
     "OK"
+}
+
+#[derive(Deserialize, Debug)]
+struct ContextParams {
+    pub scope: String,
+    pub usecase: String,
 }
 
 #[derive(Debug, Serialize)]

objectstore-client/Cargo.toml
@@ -16,7 +16,6 @@ tokio-util = { workspace = true, features = ["io"] }
 sentry = { workspace = true }
 
 [dev-dependencies]
-axum = "0.8.4"
 objectstore-server = { workspace = true }
 tempfile = { workspace = true }
 tokio = { workspace = true, features = ["macros", "full"] }

objectstore-client/src/tests.rs
@@ -3,7 +3,7 @@ use std::net::{SocketAddr, TcpListener};
 use bytes::BytesMut;
 use futures_util::TryStreamExt;
 use objectstore_server::config::{Config, Storage};
-use objectstore_server::http::make_app;
+use objectstore_server::http::App;
 use objectstore_server::state::State;
 use tempfile::TempDir;
 
@@ -102,11 +102,11 @@ impl TestServer {
         };
 
         let state = State::new(config).await.unwrap();
-        let router = make_app(state);
+        let app = App::new(state);
 
         let handle = tokio::spawn(async move {
             let listener = tokio::net::TcpListener::from_std(listener).unwrap();
-            axum::serve(listener, router).await.unwrap();
+            app.serve(listener).await.unwrap();
         });
 
         Self {

objectstore-server/Cargo.toml
@@ -33,6 +33,7 @@ tokio-stream = { workspace = true }
 tower = { version = "0.5.2" }
 tower-http = { version = "0.6.6", default-features = false, features = [
     "catch-panic",
+    "metrics",
     "set-header",
     "trace",
 ] }

objectstore-server/src/main.rs
@@ -37,10 +37,16 @@ fn main() -> Result<()> {
         .expect("Failed to install rustls crypto provider");
 
     let metrics_guard = maybe_initialize_metrics(&config)?;
+    let metrics_handle = async move {
+        match metrics_guard {
+            Some(metrics_guard) => metrics_guard.flush(None).await,
+            None => Ok(()),
+        }
+    };
 
     runtime.block_on(async move {
         let state = State::new(config).await?;
-        tokio::spawn(http::server(state));
+        let server_handle = tokio::spawn(http::server(state));
 
         elegant_departure::tokio::depart()
             .on_termination()
@@ -49,11 +55,11 @@ fn main() -> Result<()> {
             .on_signal(SignalKind::quit())
             .await;
 
-        if let Some(metrics_guard) = metrics_guard {
-            metrics_guard.flush(None).await?;
-        }
+        let (server_result, metrics_result) = tokio::join!(server_handle, metrics_handle);
+        server_result??;
+        metrics_result?;
 
-        tracing::info!("shutting down");
+        tracing::info!("shutdown complete");
 
         Ok(())
     })

commit 15e5f0ddea3d245f8a0179ea97c50ccdbf4e3eb9

fix(server): Revert accidental metric rename of requests.in_flight (#142)
Reverts an accidental regression in #140 that renames the metric for
in-flight requests.

objectstore-server/src/http.rs
@@ -126,7 +126,7 @@ impl App {
         };
 
         let emitter = in_flight_requests.run_emitter(IN_FLIGHT_INTERVAL, |count| async move {
-            merni::gauge!("server.in_flight_requests": count);
+            merni::gauge!("server.requests.in_flight": count);
         });
 
         let (serve_result, _) = tokio::join!(server, emitter);