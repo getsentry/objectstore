6fa98d418813cd710d7b3928edc16bc6ab5325cf
lcian <17258265+lcian@users.noreply.github.com>
2025-11-14 10:24:53 +0100

    improve

--- clients/rust/src/utils.rs
+++ clients/rust/src/utils.rs
@@ -1,6 +1,6 @@
 //! Utility functions that might be useful when working with Objectstore.
 
 /// Attempts to guess the MIME type from the given contents.
-pub fn guess_mime_type(contents: &[u8]) -> Option<&'static str> {
-    infer::get(contents).map(|kind| kind.mime_type())
+pub fn guess_mime_type<T: AsRef<[u8]>>(contents: T) -> Option<&'static str> {
+    infer::get(contents.as_ref()).map(|kind| kind.mime_type())
 }
