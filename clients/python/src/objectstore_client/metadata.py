fba5f7d63c733d99307d017cb3be17658b74abea
lcian <17258265+lcian@users.noreply.github.com>
2025-11-19 15:33:56 +0100

    docs

--- clients/python/src/objectstore_client/metadata.py
+++ clients/python/src/objectstore_client/metadata.py
@@ -36,6 +36,7 @@ class Metadata:
     """
     Timestamp indicating when the object was created or the last time it was replaced.
     This means that a PUT request to an existing object causes this value to be bumped.
+    This field is computed by the server, it cannot be set by clients.
     """
 
     custom: dict[str, str]
