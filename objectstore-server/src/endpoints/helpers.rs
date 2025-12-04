449ca5aa3523cd1a2cbf46ac30a35ef6f4318164
Jan Michael Auer <mail@jauer.org>
2025-12-04 14:15:45 +0100

    ref: Drop a TODO

--- objectstore-server/src/endpoints/helpers.rs
+++ objectstore-server/src/endpoints/helpers.rs
@@ -1,6 +1,5 @@
 use objectstore_service::id::ObjectId;
 
-// TODO(ja): Create axum extractors for these so we can auto-populate the scope on extraction.
 pub fn populate_sentry_scope(path: &ObjectId) {
     sentry::configure_scope(|s| {
         s.set_tag("usecase", &path.usecase);
