bb08d10d72e667a8ec2938c9248b43c1fc0cc4f8
lcian <17258265+lcian@users.noreply.github.com>
2026-01-20 15:52:28 +0100

    wip

--- objectstore-server/src/batch.rs
+++ objectstore-server/src/batch.rs
@@ -1,4 +1,2 @@
-//! Shared constants and types for batch operations.
-
 pub const HEADER_BATCH_OPERATION_KEY: &str = "x-sn-batch-operation-key";
 pub const HEADER_BATCH_OPERATION_KIND: &str = "x-sn-batch-operation-kind";
