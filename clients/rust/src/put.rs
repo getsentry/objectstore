0a87049f446bae662df7d0719194144bdf4041f8
lcian <17258265+lcian@users.noreply.github.com>
2026-03-02 10:53:56 +0100

    take asref path

--- clients/rust/src/put.rs
+++ clients/rust/src/put.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 use std::io::Cursor;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::{borrow::Cow, collections::BTreeMap};
 
 use async_compression::tokio::bufread::ZstdEncoder;
@@ -77,8 +77,9 @@ impl Session {
     /// It's therefore possible for the file to be moved or changed in the meantime.
     /// If you want to avoid this possibility, use one of the other functions to supply
     /// a payload directly instead.
-    pub fn put_file(&self, file: PathBuf) -> PutBuilder {
-        self.put_body(PutBody::File(file))
+    pub fn put_file(&self, path: impl AsRef<Path>) -> PutBuilder {
+        let path = path.as_ref().to_owned();
+        self.put_body(PutBody::File(path))
     }
 }
 
