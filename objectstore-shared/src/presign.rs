cc6aa0f8455eaf7bc747a3b8668f4c9742af49d7
lcian <17258265+lcian@users.noreply.github.com>
2026-04-10 19:03:34 +0200

    update

--- objectstore-shared/src/presign.rs
+++ objectstore-shared/src/presign.rs
@@ -96,19 +96,6 @@ mod tests {
     }
 
     #[test]
-    fn test_canonical_form_preserves_encoded_path_bytes() {
-        let canonical_slash = canonical_presigned_request(
-            "/v1/objects/test/_/folder/file.txt",
-            Some("X-Os-Expires=1712668800&X-Os-KeyId=relay-prod"),
-        );
-        let canonical_encoded_slash = canonical_presigned_request(
-            "/v1/objects/test/_/folder%2Ffile.txt",
-            Some("X-Os-Expires=1712668800&X-Os-KeyId=relay-prod"),
-        );
-        assert_ne!(canonical_slash, canonical_encoded_slash);
-    }
-
-    #[test]
     fn test_canonical_form_lowercase_hex() {
         let canonical_upper = canonical_presigned_request(
             "/v1/objects/test/org%3D1/key",
@@ -122,19 +109,7 @@ mod tests {
     }
 
     #[test]
-    fn test_canonical_form_keeps_query_encoding() {
-        let canonical = canonical_presigned_request(
-            "/path",
-            Some("X-Os-Expires=1000&foo=one%2Btwo&bar=hello+world&X-Os-KeyId=test"),
-        );
-        assert_eq!(
-            canonical,
-            "GET\n/path\nX-Os-Expires=1000&X-Os-KeyId=test&bar=hello+world&foo=one%2Btwo"
-        );
-    }
-
-    #[test]
-    fn test_canonical_form_reordered_query_params() {
+    fn test_canonical_form_reordered_params() {
         let c1 = canonical_presigned_request("/path", Some("X-Os-KeyId=test&X-Os-Expires=1000"));
         let c2 = canonical_presigned_request("/path", Some("X-Os-Expires=1000&X-Os-KeyId=test"));
         assert_eq!(c1, c2);
